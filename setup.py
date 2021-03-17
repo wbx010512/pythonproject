c1093a733713ac22a5e9eb7a4a494fe5b055137b
wbx010512 <481742959@qq.com>
2021-03-17 23:06:15 +0800

    <?xml version="1.0" encoding="UTF-8"?> <project version="4">   <component name="VcsDirectoryMappings">     <mapping directory="$PROJECT_DIR$" vcs="Git" />   </component> </project>

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/wbx010512/pythonProject.git",
     packages=setuptools.find_packages(),
     install_requires=[''],
+    license = 'MIT',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
